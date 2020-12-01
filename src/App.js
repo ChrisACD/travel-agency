1b54ea73ee230a00afc5976e175ec8e01e63e91a
Chrisdjya <djdjya@gmail.com>
2020-12-01 10:15:29 +0100

    Enter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Alert from 'react-bootstrap/Alert'
 import './App.css';
 
 function App() {
-  const show = useState(true);
   return (
-  <Alert show={show} variant="danger" dismissible>
+  <Alert variant="danger" >
     <Alert.Heading> Oh snap! You got an error</Alert.Heading>
     <p>Change this and that and try again</p>
 
